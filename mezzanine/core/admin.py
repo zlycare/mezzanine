fa98f06221b70aeb7406fc63764664cc938a9375
Evan Liu <hmisty@gmail.com>
2017-09-21 10:35:30 +0800

    实现按热门关键字过滤

--- mezzanine/core/admin.py
+++ mezzanine/core/admin.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from __future__ import unicode_literals
 
 from copy import deepcopy
@@ -71,6 +72,74 @@ class DisplayableAdminForm(ModelForm):
                                     "is set to published."))
         return content
 
+# TODO 之后考虑把这个filter抽出去
+from django.contrib.admin import SimpleListFilter
+from django.db.models import Count
+
+
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Model, Count
+
+from mezzanine.generic.models import AssignedKeyword, Keyword
+
+# TODO 考虑稍后抽出去
+def hotkeywords_for(*args):
+    """
+    the not decorated version of keywords_for
+    """
+
+    # Handle a model instance.
+    if isinstance(args[0], Model):
+        obj = args[0]
+        if getattr(obj, "content_model", None):
+            obj = obj.get_content_model()
+        keywords_name = obj.get_keywordsfield_name()
+        keywords_queryset = getattr(obj, keywords_name).all()
+        # Keywords may have been prefetched already. If not, we
+        # need select_related for the actual keywords.
+        prefetched = getattr(obj, "_prefetched_objects_cache", {})
+        if keywords_name not in prefetched:
+            keywords_queryset = keywords_queryset.select_related("keyword")
+        return [assigned.keyword for assigned in keywords_queryset]
+
+    # Handle a model class.
+    try:
+        app_label, model = args[0].split(".", 1)
+    except ValueError:
+        return []
+
+    content_type = ContentType.objects.get(app_label=app_label, model=model)
+    assigned = AssignedKeyword.objects.filter(content_type=content_type)
+    keywords = Keyword.objects.filter(assignments__in=assigned)
+    keywords = keywords.annotate(item_count=Count("assignments")).order_by('item_count').reverse()
+    if not keywords:
+        return []
+    counts = [keyword.item_count for keyword in keywords]
+    min_count, max_count = min(counts), max(counts)
+    factor = (settings.TAG_CLOUD_SIZES - 1.)
+    if min_count != max_count:
+        factor /= (max_count - min_count)
+    for kywd in keywords:
+        kywd.weight = int(round((kywd.item_count - min_count) * factor)) + 1
+    #XXX return keywords #OK，我们返回前100个最热的标签／关键词，以解决关键词太多影响页面加载速度的问题：）
+    
+    return keywords[:100]
+
+class HotKeywordFilter(SimpleListFilter):
+    title = '热门关键字'; #TODO use i18n
+    parameter_name = 'hotkw';
+    
+    def lookups(self, request, model_admin):
+        model = model_admin.model
+        kws = hotkeywords_for("{0}.{1}".format(model._meta.app_label, model._meta.object_name).lower())
+        return [(kw.id, "{}({})".format(kw.title, kw.item_count)) for kw in kws]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(keywords__keyword__exact=self.value())
+        else:
+            return queryset.all()
+
 
 class DisplayableAdmin(BaseTranslationModelAdmin):
     """
@@ -81,7 +150,7 @@ class DisplayableAdmin(BaseTranslationModelAdmin):
     list_display_links = ("title",)
     list_editable = ("status",)
     #XXX list_filter = ("status", "keywords__keyword")
-    list_filter = ("status",)
+    list_filter = ("status", HotKeywordFilter)
     # modeltranslation breaks date hierarchy links, see:
     # https://github.com/deschler/django-modeltranslation/issues/324
     # Once that's resolved we can restore this.
